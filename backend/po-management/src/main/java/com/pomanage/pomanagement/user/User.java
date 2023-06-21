d05b01036536eca3f986bfda0658f312b7cc11c3
Gideon Fu <fuzhonglong@gmail.com>
2023-06-21 14:18:44 +0800

    update

--- backend/po-management/src/main/java/com/pomanage/pomanagement/user/User.java
+++ backend/po-management/src/main/java/com/pomanage/pomanagement/user/User.java
@@ -1,8 +1,6 @@
 package com.pomanage.pomanagement.user;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
+import java.sql.Timestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,130 +10,75 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "Users")
 public class User {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
-  @Column(nullable = false, name = "username")
-  private String username;
+    @Column(name = "username", nullable = false)
+    private String username;
 
-  @Column(nullable = false, name = "password")
-  private String password;
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
 
-  @Column(unique = true, name = "email")
-  private String email;
+    @Column(name = "email", nullable = false)
+    private String email;
 
-  @Column(nullable = false, name = "role")
-  private String role;
+    @Column(name = "role", nullable = false)
+    private String role = "User";
 
-  @CreationTimestamp
-  @Column(name ="created_at")
-  private LocalDateTime createdAt;
+    @Column(name = "created_at")
+    private Timestamp createdAt;
 
+    // Getter and Setter methods
+    public int getId() {
+        return id;
+    }
 
-  public User() {
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public User(long id, String username, String password, String email, String role, LocalDateTime createdAt) {
-    this.id = id;
-    this.username = username;
-    this.password = password;
-    this.email = email;
-    this.role = role;
-    this.createdAt = createdAt;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public long getId() {
-    return this.id;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+    public String getPasswordHash() {
+        return passwordHash;
+    }
 
-  public String getUsername() {
-    return this.username;
-  }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public String getPassword() {
-    return this.password;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public String getRole() {
+        return role;
+    }
 
-  public String getEmail() {
-    return this.email;
-  }
+    public void setRole(String role) {
+        this.role = role;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
 
-  public String getRole() {
-    return this.role;
-  }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
-
-  public LocalDateTime getCreatedAt() {
-    return this.createdAt;
-  }
-
-  public void setCreatedAt(LocalDateTime createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public User id(long id) {
-    setId(id);
-    return this;
-  }
-
-  public User username(String username) {
-    setUsername(username);
-    return this;
-  }
-
-  public User password(String password) {
-    setPassword(password);
-    return this;
-  }
-
-  public User email(String email) {
-    setEmail(email);
-    return this;
-  }
-
-  public User role(String role) {
-    setRole(role);
-    return this;
-  }
-
-  public User createdAt(LocalDateTime createdAt) {
-    setCreatedAt(createdAt);
-    return this;
-  }
-
-  @Override
-  public String toString() {
-    return "{" +
-      " id='" + getId() + "'" +
-      ", username='" + getUsername() + "'" +
-      ", password='" + getPassword() + "'" +
-      ", email='" + getEmail() + "'" +
-      ", role='" + getRole() + "'" +
-      ", createdAt='" + getCreatedAt() + "'" +
-      "}";
-  }
-  
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
