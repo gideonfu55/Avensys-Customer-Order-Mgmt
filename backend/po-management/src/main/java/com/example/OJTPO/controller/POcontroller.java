a782b47500f5f647ab13762b7dd9882135859557
Rishanan-A <126557378+Rishanan-A@users.noreply.github.com>
2023-06-22 09:58:47 +0800

    Added firebase connection
    Removed SQL, added firebase

--- backend/po-management/src/main/java/com/example/OJTPO/controller/POcontroller.java
+++ backend/po-management/src/main/java/com/example/OJTPO/controller/POcontroller.java
@@ -3,39 +3,41 @@ package com.example.OJTPO.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.example.OJTPO.firebase.FirebaseService;
 import com.example.OJTPO.model.User;
-import com.example.OJTPO.service.UserService;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @CrossOrigin(origins = { "http://localhost:3000", "http://127.0.0.1:5555" })
 public class POcontroller {
 
     @Autowired
-    private UserService userService;
+    private FirebaseService firebaseService;
 
     @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody User loginUser) {
-        User user = userService.validateUser(loginUser.getUsername(), loginUser.getPassword());
-
-        if (user != null) {
-            return new ResponseEntity<>(user, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+    public CompletableFuture<ResponseEntity<User>> login(@RequestBody User loginUser) {
+        return firebaseService.validateUser(loginUser.getUsername(), loginUser.getPassword())
+                .thenApply(user -> {
+                    if (user != null) {
+                        return ResponseEntity.ok(user);
+                    } else {
+                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                    }
+                });
     }
 
     @GetMapping("/user/{username}")
-    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
-        User user = userService.getUserByUsername(username);
-        if (user != null) {
-            return new ResponseEntity<>(user, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    public CompletableFuture<ResponseEntity<User>> getUserByUsername(@PathVariable String username) {
+        return firebaseService.getUserByUsername(username)
+                .thenApply(user -> {
+                    if (user != null) {
+                        return ResponseEntity.ok(user);
+                    } else {
+                        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+                    }
+                });
     }
 }
--- backend/po-management/src/main/java/com/example/OJTPO/service/UserService.java
+++ backend/po-management/src/main/java/com/example/OJTPO/service/UserService.java
@@ -3,20 +3,22 @@ package com.example.OJTPO.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.OJTPO.firebase.FirebaseService;
 import com.example.OJTPO.model.User;
-import com.example.OJTPO.repository.UserRepository;
+
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private FirebaseService firebaseService;
 
-    public User validateUser(String username, String password) {
-        return userRepository.findByUsernameAndPassword(username, password);
+    public User validateUser(String username, String password) throws ExecutionException, InterruptedException {
+        return firebaseService.validateUser(username, password).get();
     }
 
-    public User getUserByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public User getUserByUsername(String username) throws ExecutionException, InterruptedException {
+        return firebaseService.getUserByUsername(username).get();
     }
 }
