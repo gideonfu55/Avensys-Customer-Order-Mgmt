5c8be7e2573ed347f659ec1f2a55341273a3a4a3
Rishanan-A <126557378+Rishanan-A@users.noreply.github.com>
2023-06-23 12:06:09 +0800

    Added compatibility with Gideon's code

--- backend/po-management/src/main/java/com/example/OJTPO/firebase/FirebaseService.java
+++ backend/po-management/src/main/java/com/example/OJTPO/firebase/FirebaseService.java
@@ -1,16 +1,15 @@
 package com.example.OJTPO.firebase;
 
-import com.example.OJTPO.model.User;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.*;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
 
 @Service
 public class FirebaseService {
@@ -35,103 +34,7 @@ public class FirebaseService {
         }
     }
 
-    public CompletableFuture<User> validateUser(String username, String password) {
-        CompletableFuture<User> completableFuture = new CompletableFuture<>();
-
-        getUserByUsername(username).thenAccept(user -> {
-            if (user != null && user.getPassword().equals(password)) {
-                completableFuture.complete(user);
-            } else {
-                completableFuture.complete(null);
-            }
-        }).exceptionally(ex -> {
-            completableFuture.completeExceptionally(ex);
-            return null;
-        });
-
-        return completableFuture;
-    }
-
-    public CompletableFuture<User> getUserByUsername(String username) {
-        CompletableFuture<User> completableFuture = new CompletableFuture<>();
-        DatabaseReference usersRef = firebase.child("users");
-
-        usersRef.orderByChild("username")
-                .equalTo(username)
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.exists()) {
-                            for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                                User user = userSnapshot.getValue(User.class);
-                                completableFuture.complete(user);
-                                return;
-                            }
-                        }
-                        completableFuture.complete(null);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        completableFuture.completeExceptionally(databaseError.toException());
-                    }
-                });
-        return completableFuture;
-    }
-
-    public CompletableFuture<User> createUser(User user) {
-        CompletableFuture<User> completableFuture = new CompletableFuture<>();
-        DatabaseReference usersRef = firebase.child("users");
-        DatabaseReference idRef = firebase.child("lastUserId");
-
-        idRef.runTransaction(new Transaction.Handler() {
-            @Override
-            public Transaction.Result doTransaction(MutableData currentData) {
-                if (currentData.getValue() == null) {
-                    currentData.setValue(1);
-                } else {
-                    currentData.setValue((Long) currentData.getValue() + 1);
-                }
-                return Transaction.success(currentData);
-            }
-
-            @Override
-            public void onComplete(DatabaseError databaseError, boolean committed, DataSnapshot dataSnapshot) {
-                if (committed) {
-                	user.setId(dataSnapshot.getValue(Long.class).intValue());
-
-                    usersRef.push().setValue(user, (error, ref) -> {
-                        if (error == null) {
-                            completableFuture.complete(user);
-                        } else {
-                            completableFuture.completeExceptionally(error.toException());
-                        }
-                    });
-                } else {
-                    completableFuture.completeExceptionally(databaseError.toException());
-                }
-            }
-        });
-
-        return completableFuture;
-    }
-
-
-    public CompletableFuture<Boolean> existsByUsername(String username) {
-        CompletableFuture<Boolean> completableFuture = new CompletableFuture<>();
-        DatabaseReference userRef = firebase.child("users").child(username);
-
-        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                completableFuture.complete(dataSnapshot.exists());
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                completableFuture.completeExceptionally(databaseError.toException());
-            }
-        });
-        return completableFuture;
+    public DatabaseReference getFirebase() {
+        return firebase;
     }
 }
